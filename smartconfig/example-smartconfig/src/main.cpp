681d55dc6ec6ce3202b0e3baa41f83866b8206e5
thiminhnhut <thiminhnhut@gmail.com>
2017-07-01 15:30:38 +0700

    Smart Config - Chua nho WiFi

---
+++ smartconfig/example-smartconfig/src/main.cpp
@@ -0,0 +1,49 @@
+// Atom Editor with PlatformIO
+#include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <WiFiUdp.h>
+
+void SmartConfigESP8266();
+void PrintWiFiStatus();
+
+void setup(){
+    Serial.begin(115200);
+    SmartConfigESP8266();
+}
+
+void loop(){
+    //PrintWiFiStatus();
+}
+
+
+void SmartConfigESP8266(){
+    /* Function: Ket noi WiFi qua ESP Touch
+     */
+    WiFi.mode(WIFI_STA);
+    delay(500);
+
+    while(WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+        WiFi.beginSmartConfig();
+        if(WiFi.smartConfigDone()){
+            Serial.println("WiFi Smart Config Done.");
+        }
+    }
+    Serial.println("");
+    Serial.println("");
+    WiFi.printDiag(Serial);
+    Serial.println(WiFi.localIP());
+}
+
+void PrintWiFiStatus(){
+    /* Function: In thong tin NetWork
+     */
+    Serial.printf("Connected to: ");
+    Serial.print(WiFi.SSID());
+    Serial.print(" Password: ");
+    Serial.println(WiFi.psk());
+
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
